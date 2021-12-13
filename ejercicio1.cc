f3e40aeb095d1a5b2735f4a5bd78f4f4fec0a92f
patric16UCM <93123535+patric16UCM@users.noreply.github.com>
2021-12-13 10:25:53 +0100

    Update ejercicio1.cc

--- ejercicio1.cc
+++ ejercicio1.cc
@@ -10,9 +10,9 @@ socklen_t resultado = res.ai_addr;
 for(;resultado != NULL;resultado.ai_next){
 
 getnameinfo(resultado, res.ai_addrlen,host,NI_MAXHOST,serv, NI_MAXSERV, NI_NUMERICHOST);
-printf("Direccion numerica IP %d \n",HOST);
-printf("Familia de protocolos \n",addrinfo.ai_family);//DEL PRIMERO
-printf("Tipo de socket \n",addrinfo.ai_socktype);//DEL PRIMERO
+printf("Direccion numerica IP %d \n",host);
+printf("Familia de protocolos \n",resultado.ai_family);//DEL PRIMERO
+printf("Tipo de socket \n",resultado.ai_socktype);//DEL PRIMERO
 }
 return 0;
 }
